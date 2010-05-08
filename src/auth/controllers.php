5cc35922133e862351447dd5b3ea5c36e1c2faf8
Julien Jouvent-Hallé <julienhalle@gmail.com>
2010-05-08 15:40:48 -0400

    removed js files

--- src/auth/controllers.php
+++ src/auth/controllers.php
@@ -1,12 +1,10 @@
 <?php
 
-function login_user()
-{
-    if(is_post())
-    {
+function login_user() {
+    if(is_post()) {
         $session =  get_session();
 
-        if($session->login($_POST['USERNAME'], $_POST['PASSWORD'])){
+        if($session->login($_POST['USERNAME'], $_POST['PASSWORD'])) {
             return redirect('/');
         }
         $error_message = "Invalid email and/or password. Please check your entry.";
@@ -14,8 +12,7 @@ function login_user()
     return render('login.tpl',array('error'=>$error_message));
 }
 
-function logout_user()
-{
+function logout_user() {
     $session =  get_session();
     $session->logout();
     return redirect('/');
