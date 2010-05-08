16acec35e4a0e8479f80be5059a051fa2452c801
Julien Jouvent-Hallé <julienhalle@gmail.com>
2010-05-08 13:17:20 -0400

    dynamized static pages ;-)

--- src/flatpages/controllers.php
+++ src/flatpages/controllers.php
@@ -1,25 +1,5 @@
 <?php
 
-function index()
-{
-    return render('home.tpl',array());
-}
-
-function about()
-{
-    return render('about.tpl',array());
-}
-
-function images()
-{
-    return render('images.tpl',array());
-}
-
-function news()
-{
-    return render('news.tpl',array());
-}
-
-function rules($lang){
-    return render('rules.tpl',array());
+function page($page) {
+    return render("$page.tpl");
 }
