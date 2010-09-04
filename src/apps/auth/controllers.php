4077f2ac634b54a3f7f2a12de680c3021306ccea
Julien Jouvent-Halle <julienhalle@gmail.com>
2010-09-03 21:39:10 -0400

    clean the mess ;-)

--- src/apps/auth/controllers.php
+++ src/apps/auth/controllers.php
@@ -1,19 +1,47 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
-function login_user() {
+/**
+ * auth/controllers.php
+ *
+ * PHP version 5
+ *
+ * @category Controllee
+ * @package  Auth_Controller
+ * @author   Julien Jouvent-Halle <julienhalle@heptacube.com>
+ * @license  http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link     http://github.com/jouvent/Genitura
+ * @since    0.0.2
+ */
+
+/**
+ * login_user 
+ * 
+ * @access public
+ * @return string
+ */
+function login_user()
+{
     $data = new array();
-    if(is_post()) {
+    if (is_post()) {
         $session =  get_session();
 
-        if($session->login($_POST['USERNAME'], $_POST['PASSWORD'])) {
+        if ($session->login($_POST['USERNAME'], $_POST['PASSWORD'])) {
             return redirect('/');
         }
         $data['error'] = "Invalid email and/or password. Please check your entry.";
     }
-    return render('login.tpl',$data);
+    return render('login.tpl', $data);
 }
 
-function logout_user() {
+/**
+ * logout_user 
+ * 
+ * @access public
+ * @return string
+ */
+function logout_user()
+{
     $session =  get_session();
     $session->logout();
     return redirect('/');
