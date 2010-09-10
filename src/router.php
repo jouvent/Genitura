0848340300b983a5f92045fffccc188eef75335c
Julien Jouvent-Hallé <julienhalle@gmail.com>
2010-09-10 17:41:36 -0400

    add comments

--- src/router.php
+++ src/router.php
@@ -78,12 +78,34 @@ class Router
 
 }
 
+
+/**
+ * tell the function to run on matching url
+ *
+ * @category   Routing
+ * @package    Core
+ * @subpackage Core_Route
+ * @author     Julien Jouvent-Halle <julienhalle@heptacube.com>
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link       http://github.com/jouvent/Genitura
+ * @since      0.0.2
+ */
 class Route
 {
     private $_patern;
     private $_location;
     private $_options;
 
+    /**
+     * Route 
+     * 
+     * @param string $patern   the patern to match
+     * @param mixed  $location the function location
+     * @param array  $options  additionnal options to return
+     *
+     * @access public
+     * @return void
+     */
     public function Route( $patern, $location, $options = array())
     {
         $this->_patern = '/'.str_replace('/', '\\/', $patern).'/';
@@ -92,8 +114,15 @@ class Route
     }
 
     /**
+     * match
+     *
      * return false on non matching patern, 
      * return matched param (only associatives ones) if match
+     *
+     * @param string $url the url to match
+     *
+     * @access public
+     * @return boolean
      */
     public function match($url)
     {
@@ -125,11 +154,23 @@ class Route
         return is_string($this->_location);
     }
 
+    /**
+     * getLocation 
+     * 
+     * @access public
+     * @return string
+     */
     public function getLocation()
     {
         return $this->_location;
     }
 
+    /**
+     * getOptions 
+     * 
+     * @access public
+     * @return array
+     */
     public function getOptions()
     {
         return $this->_options;
@@ -140,6 +181,8 @@ class Route
      *
      * remove what alredy traveled from the url
      *
+     * @param string $url the initial url
+     *
      * @return string what's left
      */
     public function simplify($url)
@@ -149,18 +192,44 @@ class Route
     }
 }
 
+/**
+ * store a file paht and include it on demand
+ *
+ * @category   Routing
+ * @package    Core
+ * @subpackage Core_Route
+ * @author     Julien Jouvent-Halle <julienhalle@heptacube.com>
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link       http://github.com/jouvent/Genitura
+ * @since      0.0.2
+ */
 class RouteLoader
 {
     private $_path;
     private $_var_name;
     private $_routes = null;
 
+    /**
+     * RouteLoader 
+     * 
+     * @param string $path     the file path to include
+     * @param string $var_name the variable name to return
+     *
+     * @access public
+     * @return void
+     */
     public function RouteLoader($path, $var_name = 'urls')
     {
         $this->_path = $path;
         $this->_var_name = $var_name;
     }
 
+    /**
+     * getRoutes 
+     * 
+     * @access public
+     * @return mixed
+     */
     public function getRoutes()
     {
         if ($this->_routes === null) {
@@ -179,16 +248,51 @@ class RouteLoader
     }
 }
 
+/**
+ * router 
+ * 
+ * convinient wrapper to create router
+ *
+ * @param string $url      the url to match against
+ * @param string $path     the urls file path
+ * @param string $var_name the urls list var name
+ *
+ * @access public
+ * @return Router
+ */
 function router($url, $path, $var_name = 'urls')
 {
     return new Router($url, new RouteLoader($path, $var_name));
 }
 
+/**
+ * route 
+ * 
+ * convinient wrapper to create route
+ *
+ * @param string $patern   the patern to match
+ * @param string $location the function location
+ * @param array  $options  additional options to return
+ *
+ * @access public
+ * @return Route
+ */
 function route($patern, $location, $options =array() )
 {
     return new Route($patern, $location, $options);
 }
 
+/**
+ * routes 
+ * 
+ * convinient wrapper to create route loader
+ *
+ * @param string $path     the file to load 
+ * @param string $var_name the var to extract
+ *
+ * @access public
+ * @return RouteLoader
+ */
 function routes($path, $var_name = 'urls')
 {
     return new RouteLoader($path, $var_name);
