9a444a32ce357cdfb77b9793976bf7f24c993075
Julien Jouvent-Halle <julienhalle@gmail.com>
2010-09-11 10:00:01 -0400

    cleaning the mess

--- src/include/helpers/errors.php
+++ src/include/helpers/errors.php
@@ -1,19 +1,46 @@
 <?php
+/* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
+
+/**
+ * hepres/errors.php
+ *
+ * PHP version 5
+ *
+ * @category   Helpers
+ * @package    Core
+ * @subpackage Core_Helpers
+ * @author     Julien Jouvent-Halle <julienhalle@heptacube.com>
+ * @license    http://www.opensource.org/licenses/mit-license.php MIT License
+ * @link       http://github.com/jouvent/Genitura
+ * @since      0.0.2
+ */
+
+/**
+ * format_error 
+ * 
+ * @param mixed $errors a Doctrine error stack as an array
+ *
+ * @access public
+ * @return void
+ */
 function format_error($errors)
 {
     $formated_errors = array();
-    foreach($errors as $field => $field_errors)
-    {
-        $fliped = array_flip($field_errors);
-        foreach($fliped as $key=>$value)
-        {
-            $fliped[$key] = 1;
-        }
+    foreach ($errors as $field => $field_errors) {
+        $fliped = array_fill_keys($field_errors, 1);
         $formated_errors[$field] = $fliped;
     }
     return $formated_errors;
 }
 
+/**
+ * get_errors 
+ * 
+ * @param mixed $record a Doctrine model 
+ *
+ * @access public
+ * @return void
+ */
 function get_errors($record)
 {
     $record->isValid();
